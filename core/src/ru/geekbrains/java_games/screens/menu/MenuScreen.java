be8dba67f0df4a660a16655b7789dee220b1d6d7
Stan402 <konvin@yahoo.com>
2017-08-30 13:00:21 +0300

    Star added

--- core/src/ru/geekbrains/java_games/screens/menu/MenuScreen.java
+++ core/src/ru/geekbrains/java_games/screens/menu/MenuScreen.java
@@ -6,22 +6,29 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
 import java.util.Locale;
 
+import ru.geekbrains.java_games.screens.stars.Star;
 import ru.geekuniversity.engine.Base2DScreen;
 import ru.geekuniversity.engine.Sprite2DTexture;
 import ru.geekuniversity.engine.math.Rect;
+import ru.geekuniversity.engine.math.Rnd;
 import ru.geekuniversity.engine.sprites.Sprite;
 
 
 public class MenuScreen extends Base2DScreen {
 
+    private static final float STAR_WIDTH = 0.01f;
+
     private Sprite2DTexture textureBackground;
+    private TextureAtlas atlas;
     private Background background;
+    private Star star;
 
 
     public MenuScreen(Game game) {
@@ -32,27 +39,44 @@ public class MenuScreen extends Base2DScreen {
     public void show() {
         super.show();
         textureBackground = new Sprite2DTexture("textures/bg.png");
+        atlas = new TextureAtlas("textures/mainAtlas.pack");
         background = new Background(new TextureRegion(textureBackground));
+        TextureRegion regionStar = atlas.findRegion("star");
+        float vx = Rnd.nextFloat(-0.005f, 0.005f);
+        float vy = Rnd.nextFloat(-0.05f, -0.1f);
+        float starWidth = STAR_WIDTH * Rnd.nextFloat(0.75f, 1f);
+        star = new Star(regionStar, vx, vy, starWidth);
     }
 
     @Override
     protected void resize(Rect worldBounds) {
         background.resize(worldBounds);
+        star.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
+        update(delta);
+        draw();
+    }
+
+    private void update(float deltaTime){
+        star.update(deltaTime);
+    }
+
+    private void draw(){
         Gdx.gl.glClearColor(0.7f, 0.7f, 0.7f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         background.draw(batch);
-
+        star.draw(batch);
         batch.end();
     }
 
     @Override
     public void dispose () {
         textureBackground.dispose();
+        atlas.dispose();
         super.dispose();
     }
 
